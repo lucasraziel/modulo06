60ed188bcd80b603b8fdec05cf25c0bd87d09c7f
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-02 22:12:10 -0300

    aula06 ReactNavigation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import './config/ReactotronConfig';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View>
-    );
-  }
-}
+import Routes from './routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+export default function App() {
+  return <Routes />;
+}
